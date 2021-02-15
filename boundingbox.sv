b0c39663d8ccf302222a41d990ab2fc52c1fa071
Hung Nguyen <hunglnl01@gmail.com>
2021-02-14 20:56:21 -0800

    adding modules

---
+++ boundingbox.sv
@@ -0,0 +1,17 @@
+module boundingBox(input logic clk, input logic rst_n,
+            input logic en, output logic rdy,
+            output logic [7:0] addr, output logic [7:0] wrdata, output logic wren,
+            output logic[10:0] xMin, output logic[10:0] xMax,
+            output logic[10:0] yMin, output logic[10:0] yMax);
+
+
+
+
+
+
+
+
+
+, 
+
+
