dc3e0d09fbd36f8e680f3a23bd5d5985d1ffc8a4
nathan-n-poon <nathannpoon@gmail.com>
2021-02-18 22:17:57 -0700

    tests

--- boundingBox/tb_boundingbox.sv
+++ boundingBox/tb_boundingbox.sv
@@ -16,7 +16,7 @@ module tb_boundingBox();
         #20;
         // D:/main/2020fall/poo/beep-boop/kodim24.hex
         // $readmemh("D:/main/2020fall/poo/beep-boop/kodim24.hex", dut.ram);
-        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/test.hex", dut.ram);
+        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/square.hex", dut.ram);
         #20;
 
         
@@ -29,6 +29,62 @@ module tb_boundingBox();
         begin
             #10;
         end
+
+        assert(dut.xMin == 28 && dut.yMin == 29 && dut.xMax == 79 && dut.yMax == 65)
+        else
+        begin
+            $display("expected values: 28 29 79 65 \n");
+            $display("actual values: %d %d %d %d \n", dut.xMin, dut.yMin, dut.xMax, dut.yMax);
+            $stop;
+        end
+
+        #20;
+        // D:/main/2020fall/poo/beep-boop/kodim24.hex
+        // $readmemh("D:/main/2020fall/poo/beep-boop/kodim24.hex", dut.ram);
+        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/triangle.hex", dut.ram);
+        #20;
+
+        
+        KEY[3] = 1'b0;
+        #10;
+        KEY[3] = 1'b1;
+        #200;
+
+        while(~dut.done)
+        begin
+            #10;
+        end
+
+        assert(dut.xMin == 28 && dut.yMin == 34 && dut.xMax == 69 && dut.yMax == 78)
+                else
+        begin
+            $stop;
+        end
+
+        #20;
+        // D:/main/2020fall/poo/beep-boop/kodim24.hex
+        // $readmemh("D:/main/2020fall/poo/beep-boop/kodim24.hex", dut.ram);
+        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/circle.hex", dut.ram);
+        #20;
+
+        
+        KEY[3] = 1'b0;
+        #10;
+        KEY[3] = 1'b1;
+        #200;
+
+        while(~dut.done)
+        begin
+            #10;
+        end
+
+        assert(dut.xMin == 27 && dut.yMin == 27 && dut.xMax == 81 && dut.yMax == 78)
+                else
+        begin
+            $stop;
+        end
+
+        
 
         $display("done!");
         $stop;
---
+++ cropping/tb_cropping.sv
@@ -0,0 +1,59 @@
+module tb_cropping();
+    logic CLOCK_50;
+    logic [3:0] KEY;
+
+    // instiate boundingBoxTop
+    cropping dut(.CLOCK_50(CLOCK_50), .KEY(KEY));
+
+    // clock cycle
+    initial begin
+        CLOCK_50 = 1'b0; 
+        forever begin #10 CLOCK_50 = ~CLOCK_50;  end
+    end
+
+    initial begin
+        // reset
+        #20;
+        // D:/main/2020fall/poo/beep-boop/kodim24.hex
+        // $readmemh("D:/main/2020fall/poo/beep-boop/kodim24.hex", dut.ram);
+        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/test.hex", dut.ram);
+        #20;
+
+        
+        KEY[3] = 1'b0;
+        #10;
+        KEY[3] = 1'b1;
+        #200;
+
+        while(~dut.done)
+        begin
+            #10;
+        end
+
+        $display("done!");
+        $stop;
+
+        
+    end
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
