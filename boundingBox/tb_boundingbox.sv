722961ac10a32aedda7d4976ccc04c2826693c16
nathan-n-poon <nathannpoon@gmail.com>
2021-02-18 23:49:20 -0700

    more tests

--- boundingBox/tb_boundingbox.sv
+++ boundingBox/tb_boundingbox.sv
@@ -88,6 +88,83 @@ module tb_boundingBox();
             // $stop;
         end
 
+        #20;
+        // D:/main/2020fall/poo/beep-boop/kodim24.hex
+        // $readmemh("D:/main/2020fall/poo/beep-boop/kodim24.hex", dut.ram);
+        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/shape.hex", dut.ram);
+        #20;
+
+        
+        KEY[3] = 1'b0;
+        #20;
+        KEY[3] = 1'b1;
+        #200;
+
+        while(~dut.done)
+        begin
+            #10;
+        end
+
+        assert(dut.xMin == 4 && dut.yMin == 16 && dut.xMax == 84 && dut.yMax == 77)
+                else
+        begin
+            $display("expected values: 4 16 84 77 \n");
+            $display("actual values: %d %d %d %d \n", dut.xMin, dut.yMin, dut.xMax, dut.yMax);
+            // $stop;
+        end
+
+        #20;
+        // D:/main/2020fall/poo/beep-boop/kodim24.hex
+        // $readmemh("D:/main/2020fall/poo/beep-boop/kodim24.hex", dut.ram);
+        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/bottomRight.hex", dut.ram);
+        #20;
+
+        
+        KEY[3] = 1'b0;
+        #20;
+        KEY[3] = 1'b1;
+        #200;
+
+        while(~dut.done)
+        begin
+            #10;
+        end
+
+        assert(dut.xMin == 99 && dut.yMin == 99 && dut.xMax == 99 && dut.yMax == 99)
+                else
+        begin
+            $display("expected values: 99 99 99 99 \n");
+            $display("actual values: %d %d %d %d \n", dut.xMin, dut.yMin, dut.xMax, dut.yMax);
+            // $stop;
+        end
+
+
+                #20;
+        // D:/main/2020fall/poo/beep-boop/kodim24.hex
+        // $readmemh("D:/main/2020fall/poo/beep-boop/kodim24.hex", dut.ram);
+        $readmemh("C:/Users/natha/OneDrive/Documents/MATLAB/topLeft.hex", dut.ram);
+        #20;
+
+        
+        KEY[3] = 1'b0;
+        #20;
+        KEY[3] = 1'b1;
+        #200;
+
+        while(~dut.done)
+        begin
+            #10;
+        end
+
+        assert(dut.xMin == 0 && dut.yMin == 0 && dut.xMax == 0 && dut.yMax == 0)
+                else
+        begin
+            $display("expected values: 0 0 0 0 \n");
+            $display("actual values: %d %d %d %d \n", dut.xMin, dut.yMin, dut.xMax, dut.yMax);
+            // $stop;
+        end
+        
+
         
 
         $display("done!");
